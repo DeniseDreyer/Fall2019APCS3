239f2687e51ba323fd605430104e242e078280de
DeniseDreyer <ddreyer@millshighsch.org>
2019-08-30 11:27:55 -0700

    part 1

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -7,8 +7,8 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		
-
+		System.out.println(Calculate.square(2));
+		System.out.println(Calculate.cube(2));
 	}
 
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,7 +4,23 @@
  * August 29, 2019
  */
 public class Calculate {
+	//A call to square returns the square of the value passed.  The method accepts an integer and returns an integer
 	public static int square(int number) {
-		return number * number;
+		int answer=0;
+		answer = number * number;
+		return answer;
 	}
+	
+	//A call to cube returns the cube of the value passed.  The method accepts an integer and returns an integer
+	public static int cube(int number) {
+		int answer=0;
+		answer = number * number*number;
+		return answer;
+	}
+	//A call to average returns the average of the values passed to it.  The method accepts two doubles and returns a double
+		public static int average(int number) {
+			int answer=0;
+			answer = number * number*number;
+			return answer;
+		}
 }
